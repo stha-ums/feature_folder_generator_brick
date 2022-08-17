5f546b7e45c34eefb551911e9ec2597a6283a37c
Umesh Shrestha <stha.ums@gmail.com>
2022-08-17 09:37:50 +0545

    Simple Clean Code brick

---
+++ simple_clean_code/ffg/__brick__/{{feature_name.snakeCase()}}/presentation/screens/{{feature_name.snakeCase()}}_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class {{feature_name.pascalCase()}} extends StatelessWidget {
+  const {{feature_name.pascalCase()}}({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
